9b4101608ee80b26084b918d0185ff6bcb6848bb
supra-matucasal <m.casal@supraoracles.com>
2024-07-19 12:18:41 +0200

    Comment middleware for the moment

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,37 +6,37 @@ const allowedOrigins = [
 
 export async function middleware(req: NextRequest) {
 
-    console.log('Req: ', req.url)
+  //   console.log('Req: ', req.url)
 
-  let referer = req.headers.get('Referer');
-  console.log('referer: ', referer)
+  // let referer = req.headers.get('Referer');
+  // console.log('referer: ', referer)
 
-  if (!referer) {
-    return NextResponse.redirect('http://supra.com');
-  }
-  const url = new URL(referer);
-  const origin = url.origin;
+  // if (!referer) {
+  //   return NextResponse.redirect('http://supra.com');
+  // }
+  // const url = new URL(referer);
+  // const origin = url.origin;
 
   
-  console.log('origin: ', origin)
-  console.log('allowedOrigins: ', allowedOrigins)
+  // console.log('origin: ', origin)
+  // console.log('allowedOrigins: ', allowedOrigins)
 
-  if (origin && allowedOrigins.includes(origin)) {
-    const response = NextResponse.next();
+  // if (origin && allowedOrigins.includes(origin)) {
+  //   const response = NextResponse.next();
 
-    response.headers.set('Access-Control-Allow-Origin', origin);
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token, X-Requested-With');
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
+  //   response.headers.set('Access-Control-Allow-Origin', origin);
+  //   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  //   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token, X-Requested-With');
+  //   response.headers.set('Access-Control-Allow-Credentials', 'true');
 
-    if (req.method === 'OPTIONS') {
-      return new NextResponse(null, { headers: response.headers });
-    }
+  //   if (req.method === 'OPTIONS') {
+  //     return new NextResponse(null, { headers: response.headers });
+  //   }
 
-    return NextResponse.next();
-  } else {
-    return new NextResponse('Not allowed', { status: 403, headers: { 'Content-Type': 'text/plain' } });
-  }
+  //   return NextResponse.next();
+  // } else {
+  //   return new NextResponse('Not allowed', { status: 403, headers: { 'Content-Type': 'text/plain' } });
+  // }
 
 }
 
